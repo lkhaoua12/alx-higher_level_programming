4116093a49862aa9b88f2cd5435b3a855b8006a3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-06 00:55:57 +0100

    using lookup table

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,16 +5,14 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *current = list;
-	long unsigned list_index[MAX_SIZE];
+	int list_index[MAX_SIZE];
 
 	while (current != NULL)
 	{
 		if (list_index[(long unsigned)current % MAX_SIZE])
 			return (1);
 		else
-		{
 			list_index[(long unsigned)current % MAX_SIZE] = 1;
-		}
 		current = current->next;
 	}
 	return (0);

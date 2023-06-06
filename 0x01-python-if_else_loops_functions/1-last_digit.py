0341ba483c3d031814a5d9599edf6a67949d4f48
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-06 23:31:03 +0100

    break lines over 80 char

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,16 +12,16 @@ last_digit = abs(number) % 10
 if last_digit > 5:
     if original_number > 0:
         print(f"Last digit of {original_number}"
-              f"is {last_digit} and is greater than 5")
+              f" is {last_digit} and is greater than 5")
     else:
         print(f"Last digit of {original_number} is -{last_digit}"
-              f"and is less than 6 and not 0")
+              f" and is less than 6 and not 0")
 elif last_digit == 0:
     print(f"Last digit of {original_number} is {last_digit} and is 0")
 else:
     if original_number > 0:
         print(f"Last digit of {original_number} is {last_digit}"
-              f"and is less than 6 and not 0")
+              f" and is less than 6 and not 0")
     else:
         print(f"Last digit of {original_number} is -{last_digit}"
-              f"and is less than 6 and not 0")
+              f" and is less than 6 and not 0")

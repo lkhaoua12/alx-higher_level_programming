8d240cfe69b0fa80a407e5aaefd046517c5c0271
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-06 23:16:16 +0100

    handle negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+Get the last digit of a number and check its range.
+"""
 import random
+
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print(f"Last digit of {number} is {number % 10:d} and is greater than 5")
-elif number % 10 == 0:
-    print(f"Last digit of {number} is {number % 10:d} and is zero")
+original_number = number
+
+if number < 0:
+    number = number * -1
+
+last_digit = number % 10
+
+if last_digit > 5:
+    print(f"Last digit of {original_number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {original_number} is {last_digit} and is zero")
 else:
-    print(f"Last digit of {number} is {number % 10:d} and is less than 6 and not 0")
+    print(f"Last digit of {original_number} is {last_digit} and is less than 6 and not 0")
+

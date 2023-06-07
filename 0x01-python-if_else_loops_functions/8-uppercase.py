4938e9577197f17b80aa6e740e1a5871eaedfb64
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-07 01:20:04 +0100

    check if char a lower case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(str):
-    result  = ""
+    result = ""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)))
+            result += chr(ord(i) - 32)
         else:
-            print("{}".format(i))
+            result += i
+    print(result)

b2d3f4c36886fbb54a85a8e84c0d0b4f6a7c8dc5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-08 23:34:09 +0100

    import func add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""
+import add and call it if from main.
+"""
 from add_0 import add
 a = 1
 b = 2
 if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b ,add(a, b)))
-

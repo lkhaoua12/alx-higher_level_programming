6b7e477ba0b2251849eda563db990b557620c425
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-09 02:02:06 +0100

    custom cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, subtract, multiply, divide
+from sys import exit, argv
+from calculator_1 import add, sub, mul, div
 
 def calculate(a, operator, b):
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
-        result = subtract(a, b)
+        result = sub(a, b)
     elif operator == '*':
-        result = multiply(a, b)
+        result = mul(a, b)
     elif operator == '/':
-        result = divide(a, b)
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     
     print(f"{a} {operator} {b} = {result}")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
     calculate(a, operator, b)

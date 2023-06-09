9b5d1850d1aa0bc4d7fd5a2d7103d227e595acb0
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-09 01:38:33 +0100

    add command args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    result = 0
+    args_num = len(argv)
+    for i in range(1, args_num):
+        result += int(argv[i])
+    print("{:d}".format(result))
+    

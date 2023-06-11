7b68728d8a99bc35ad6c4e1a5cf850702b36132b
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-11 23:30:26 +0100

    print elem of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return
     for i in range(0, len(my_list)):
         if i == idx:
             return i

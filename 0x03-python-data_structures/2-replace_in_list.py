4991f68a4eb6307c8e889b971e43d87f0367262c
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-11 23:34:19 +0100

    print elem of list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return
+        return my_list
     for i in range(0, len(my_list)):
         if i == idx:
-            return my_list[i]
+            my_list[i] = element

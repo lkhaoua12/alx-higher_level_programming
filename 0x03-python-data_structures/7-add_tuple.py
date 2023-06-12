206f197810722d6a2ac04657a7222f4ee3116d97
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-12 23:42:01 +0100

    remove all c

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple_a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
     new_tuple_b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
-    
     a = new_tuple_a[0] + new_tuple_b[0]
     b = new_tuple_a[1] + new_tuple_b[1]
     return a, b
-

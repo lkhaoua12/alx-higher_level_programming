2896c86f655b7bd4c7c43db804861ea736f03ba2
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-13 00:22:35 +0100

    delete at index

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,5 @@
-#!/usr/bin/pydoc3
+#!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for num in my_list:
-        new_list.append(num % 2 == 0)
+    new_list = [num % 2 == 0 for num in my_list]
     return new_list

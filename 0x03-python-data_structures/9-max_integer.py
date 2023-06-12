471214ebd05e631507af06cd8ed98b766e0f895a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-12 23:48:28 +0100

    remove all c

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max = 0
+        for num in my_list:
+            if num > max:
+                max = num
+    return max

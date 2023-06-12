7e623b13644699a45eb813f4ced05bbdb33f52fa
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-12 23:17:13 +0100

    remove all c

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,12 +7,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(num))
             else:
                 print("{:d}".format(num), end=" ")
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-if __name__ == "__main__":
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()

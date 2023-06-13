c922c1269c4facaaba4d7babcf610ab6ba0a67d5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-13 02:47:22 +0100

    handle memory leak

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -19,53 +19,47 @@ listint_t *reverseLinkedList(listint_t *head)
 
 int is_palindrome(listint_t **head)
 {
-        listint_t *slow = *head;
-        listint_t *fast = *head;
-        listint_t *prev_slow = *head;
-        listint_t *second_half = NULL;
-        int is_palindrome = 1;
-	if (*head == NULL || (*head)->next == NULL)
-		return 1;
-        if (*head != NULL && (*head)->next != NULL)
+        if (*head == NULL || (*head)->next == NULL)
         {
-                while (fast != NULL && fast->next != NULL)
-                {
-                        fast = fast->next->next;
-                        prev_slow = slow;
-                        slow = slow->next;
-                }
-
-                if (fast != NULL)
-                {
-                        slow = slow->next;
-                }
-
-                second_half = reverseLinkedList(slow);
-
-                while (second_half != NULL)
-                {
-                        if ((*head)->n != second_half->n)
-                        {
-                                is_palindrome = 0;
-                                break;
-                        }
-
-                        *head = (*head)->next;
-                        second_half = second_half->next;
-                }
-
-                reverseLinkedList(slow);
-
-                if (fast != NULL) 
-                {
-                        prev_slow->next = slow;
-                        slow->next = second_half;
-                }
-                else
-                {
-                        prev_slow->next = second_half;
-                }
+                return 1;
         }
 
+        listint_t *slow = *head;
+        listint_t *fast = *head;
+        /* listint_t *prev_slow = *head; */
+        listint_t *second_half = NULL;
+        int is_palindrome = 1;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+                fast = fast->next->next;
+                /* prev_slow = slow; */
+                slow = slow->next;
+        }
+
+        if (fast != NULL)
+        {
+                slow = slow->next;
+        }
+
+        second_half = reverseLinkedList(slow);
+
+        listint_t *temp1 = *head;
+        listint_t *temp2 = second_half;
+
+        while (temp1 != NULL && temp2 != NULL)
+        {
+                if (temp1->n != temp2->n)
+                {
+                        is_palindrome = 0;
+                        break;
+                }
+
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+        }
+
+        second_half = reverseLinkedList(second_half);
+	temp1->next = second_half;
         return is_palindrome;
 }

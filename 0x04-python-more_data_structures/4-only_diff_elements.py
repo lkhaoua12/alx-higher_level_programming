e660d8c20443b93f1e041df317e4f119f931cf16
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 00:18:15 +0100

    unique set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     uniq_set = {elem for elem in set_1 if elem not in set_2}
-    uniq_set += {elem for elem in set_2 if elem not in set_1}
+    uniq_set = uniq_set.union({elem for elem in set_2 if elem not in set_1})
     return uniq_set

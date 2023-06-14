12ee5e48f4f0b8d6a89d7be453040f845ea94368
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 00:05:17 +0100

    unique set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    unique_elem = set_1.intersection(set_2)
+    unique_elem = {elem for elem in set_1 if elem in set_2}
     return unique_elem
+

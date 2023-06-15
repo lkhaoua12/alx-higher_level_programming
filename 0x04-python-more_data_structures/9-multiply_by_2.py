bcfad1624b4ce411e56db9a381d801aa2682fec0
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 01:08:42 +0100

    unique set

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    b_dic = {key: value * 2 for key, value in a_dictionary.items()
+    b_dic = {key: value * 2 for key, value in a_dictionary.items()}
     return b_dic

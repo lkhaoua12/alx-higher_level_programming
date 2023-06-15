5457f388f67de57c99934b5002cd13768f663798
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 02:55:55 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,29 +4,13 @@ def roman_to_int(roman_string):
         return 0
     result = 0
     prev_value = 0
-    a_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    a_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for value in roman_string:
         if value not in a_dic.keys():
             return 0
         elif a_dic[value] > prev_value:
-            result += a_dic[value] - prev_value * 2 
+            result += a_dic[value] - prev_value * 2
         else:
-            result += a_dic[value] 
+            result += a_dic[value]
         prev_value = a_dic[value]
     return result
-
-if __name__ == "__main__":
-    roman_number = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))

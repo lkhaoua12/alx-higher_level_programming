8c0f1284e5bb09932260aa4ef3c75bbc3ac72771
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 01:27:50 +0100

    unique set

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     result = None
     for key in a_dictionary.keys():
-        if a_dictionary[key] > result:
+        if a_dictionary[key] > a_dictionary[result]:
             result = key
     return result

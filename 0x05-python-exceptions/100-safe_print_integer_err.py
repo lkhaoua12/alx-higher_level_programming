deb9ae2d897e432134d809f465c0ae6a744773bc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-28 00:38:51 +0100

    use of exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,10 +4,10 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}\n".format(value))
+        print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}\n".format(e), file=sys.stderr)
+        print(e, file=sys.stderr)
         return False
 
 

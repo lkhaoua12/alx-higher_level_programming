b4dfeee0b9c4811e6e2580db04a66dc7218c8c40
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-30 09:50:09 +0100

    set size attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+define square with size attribute
+"""
 class Square:
+    """
+    Square: a calss that define a squre
+    attribute: none
+    method: none
+    """
     __init__(self, size):
         self.__size = size

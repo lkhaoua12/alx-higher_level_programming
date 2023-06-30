ec8a1b6a9d2e2c591a806370cfbd64d059c99368
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-30 10:02:15 +0100

    add size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+define class with instance att size
+"""
+
+
+class Square:
+    """
+    define a square with instance att size
+    Attribute: size
+    method: none
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

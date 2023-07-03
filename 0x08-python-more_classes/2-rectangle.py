d27104d217d69ee1990a95b64600805cb0076459
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-03 23:23:51 +0100

    rectangle height and width setter and getter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,4 +44,4 @@ class Rectangle:
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
-        return self.__height * self.__width
+        return 2 * (self.__height + self.__width)

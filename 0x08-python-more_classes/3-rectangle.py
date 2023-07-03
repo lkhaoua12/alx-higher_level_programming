7b2cd1e90ea5dde91e417ad8016ef53a7f0ea406
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-03 23:45:35 +0100

    rectangle height and width setter and getter

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,4 +52,18 @@ class Rectangle:
         rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
-        return rectangle_str
+        return rectangle_str[:-1]
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    print(str(my_rectangle))
+    print(repr(my_rectangle))
+
+    print("--")
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle)
+    print(repr(my_rectangle))

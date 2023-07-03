50bd43767615f29400e9abe110bff0c28eb98c9b
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-03 23:33:08 +0100

    rectangle height and width setter and getter

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        rec_str = ""
-        for i in rangle(self.__height):
-            rec_str += "#" * self.__width + "\n"
-        return rec_str
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += "#" * self.__width + "\n"
+        return rectangle_str

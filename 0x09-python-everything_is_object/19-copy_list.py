0e863a65155183eb8ae8dee7a039130dea2f2e71
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-06 00:47:47 +0100

    identifer

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this module conatain a function that copies a list
+"""
+
+def copy_list(l):
+    """
+    this function return a copy of a list
+    """
+
+    return l[:]

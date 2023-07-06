56c842df88ef61ae6d4e519962d5e8eccb309a80
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-06 11:33:48 +0100

    counter atr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+this module declare a class and set it attribute
+"""
+
 class LockedClass:
+    """
+    a locked class from dynamicaly changing attribute
+    """
+
     def __setattr__(self, name, value):
         if not hasattr(self, name) and name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))

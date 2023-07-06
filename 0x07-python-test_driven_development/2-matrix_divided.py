690277645a0911ebc58fc56de00dc9e0d9e5f816
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-07 00:27:38 +0100

    divid matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,8 @@ def matrix_divided(matrix, div):
     """
 
     if type(matrix) is not list or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of \
+        integers/floats")
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
@@ -24,7 +25,3 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     return [[round(elem / div, 2)for elem in row] for row in matrix]
-
-    
-
-

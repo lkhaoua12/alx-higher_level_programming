2dedc80ad05f1d65a630b2dffc617e1e6aa41e5d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-06 11:41:51 +0100

    identifer

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,7 +11,10 @@ class LockedClass:
 
     def __setattr__(self, name, value):
         if not hasattr(self, name) and name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute
-                                 '{}'".format(name))
+            raise AttributeError(
+                    "'LockedClass' object has no attribute '{}'"
+                    .format(name)
+            )
+
         else:
             object.__setattr__(self, name, value)

2991d444dd3eeec9146f614436bb7ca5aed3015c
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-08 00:15:00 +0100

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this module define a function that print a text
+"""
+
+
+def text_indentation(text):
+    """
+    this function takes a text and re-formate it.
+    Args: text(string)
+    """
+
+    deli = [":", ":", "."]
+    line = ""
+    result = ""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in text:
+        line += char
+
+        if char in deli:
+            result += line + "\n\n"
+            line = ""
+
+    if line:
+        result += line
+
+    print(result)

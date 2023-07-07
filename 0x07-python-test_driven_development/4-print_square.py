2492d271248cc8714b7c8d6b0c41a509b80b111f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-07 01:48:51 +0100

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+this module define a function that print a square
+"""
+
+
+def print_square(size):
+    """
+    this function print a square
+    Args: size(int)
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+
+
+if __name__ == "__main__":
+    print_square(4)
+    print("")
+    print_square(10)
+    print("")
+    print_square(0)
+    print("")
+    print_square(1)
+    print("")
+    try:
+        print_square(-1)
+    except Exception as e:
+        print(e)
+    print("")

1ae74b1233ee43e6720e2c811c78087bfa2ef2e2
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-12 00:06:24 +0100

    open file and print it

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+define a function that open file
+"""
+
+
+def read_file(filename=""):
+    """
+    read from file and print it to the stdout
+    """
+
+    with open(filename, "r", encoding="utf-8") as fd:
+        for line in fd:
+            print(line, end="")

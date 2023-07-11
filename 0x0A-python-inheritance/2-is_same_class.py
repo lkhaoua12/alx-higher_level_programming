e5ecb73005fdfe4bd60d2b3b4557ada7b617821e
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-11 02:40:25 +0100

    check for instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module define a function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    this function check if an obj in an instance
+    of a_class
+    """
+
+    return type(obj) is a_class

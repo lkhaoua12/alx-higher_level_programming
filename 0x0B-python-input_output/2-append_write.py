d6cda9ce29d1ce91e9e3e613dd156edb5e13bf75
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-12 00:20:24 +0100

    open file and print it

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,11 +4,11 @@ define a function that open file
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
     read from file and print it to the stdout
     """
 
-    with open(filename, "w", encoding="utf-8") as fd:
+    with open(filename, "a", encoding="utf-8") as fd:
         fd.write(text)
         return len(text)

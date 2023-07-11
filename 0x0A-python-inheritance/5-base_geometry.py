0e3af7f92921ea37205f08203180a4635878a867
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-11 03:24:35 +0100

    check for instance

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -4,10 +4,10 @@ this module define a function
 """
 
 
-def inherits_from(obj, a_class):
+class BaseGeometry:
     """
     this function check if an obj in an instance
     of a_class
     """
 
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    pass

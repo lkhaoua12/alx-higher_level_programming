b748f9d6796e2a3153cab9dbe4401762f555ac7f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-12 00:26:56 +0100

    open file and print it

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import json
 """
 define a function that open file
 """
+
+
+import json
+
 
 def to_json_string(my_obj):
     """
     return the JSON string of an object
     """
 
-    return json.dump(my_obj)
+    return json.dumps(my_obj)

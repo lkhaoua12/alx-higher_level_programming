59f42ff3be34e0846ff0351bfc70fcd0973f18e0
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-11 03:59:22 +0100

    check for instance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,24 +4,7 @@ this module define a function
 """
 
 
-class BaseGeometry:
-    """
-    this function check if an obj in an instance
-    of a_class
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-    def __init__(self):
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """

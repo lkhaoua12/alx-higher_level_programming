7dc2bfd1733f4c41675ca05b55c19aa67ced23dc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-12 03:44:04 +0100

    save json to file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,7 +6,7 @@ this module contains class_to_json function
 
 def class_to_json(obj):
     """
-    this function takes an object and return it 
+    this function takes an object and return it
     dict desription of data
     """
 

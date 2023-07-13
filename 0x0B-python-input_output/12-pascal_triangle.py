ae51970af08ea8b03399fcf3feb445ef91fc2bb2
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-07-13 03:18:46 +0100

    pascale triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,21 +5,13 @@ This module contains a function for generating Pascal's
 Triangle up to a given number of rows.
 """
 
+
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the specified number of rows.
 
     Args:
         n (int): The number of rows to generate in Pascal's Triangle.
-
-    Returns:
-        list: A 2-dimensional list representing Pascal's Triangle
-        up to the specified number of rows.
-              Each inner list represents a row in the triangle.
-
-    Example:
-        >>> pascal_triangle(5)
-        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
     if n <= 0:
         return []

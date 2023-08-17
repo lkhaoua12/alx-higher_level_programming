8955fd77cc58c19b3f6f2849fb4d9c99c852ee90
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-08-17 01:59:03 +0100

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- 4-first_table.sql
 -- create a table in the database
-CREATE IF NOT EXIST first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));

2a2841e12935274636c9e482b08027b554aa4c1e
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-08-17 02:16:08 +0100

    first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,4 @@
 -- 7-insert_value.sql
 -- insert a row in a database
-INSERT INTO first_table (
-	attribute(id, name)
-) VALUES ( 89, 'Best School')
+INSERT INTO first_table (id, name)
+	VALUES ( 89, 'Best School');

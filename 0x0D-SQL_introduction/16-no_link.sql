fb148c23149e06f212332ab33e86082a36530566
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-08-17 02:52:47 +0100

    groupe by func

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- 16-no_link.sql
 -- list where record isnt null
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

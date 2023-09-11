5da4d19ff273e446979347f4353078222682567d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-11 15:40:48 +0100

    fixed negative numbers

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let accur = parseInt(argv[2]);
 if (isNaN(accur)) {
   console.log('Missing number of occurrences');
 } else {
-  while (accur) {
+  while (accur > 0) {
     console.log('C is fun');
     accur--;
   }

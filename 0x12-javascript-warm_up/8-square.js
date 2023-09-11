af9a170ac09e7eb51827daf2257d21d7f4757f9b
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-11 22:14:30 +0100

    printing square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (!argv[2]) {
+  console.log('Missing size');
+} else {
+  const num = parseInt(argv[2]);
+  if (!isNaN(num)) {
+    for (let i = 0; i < num; i++) {
+      let line = '';
+      for (let j = 0; j < num; j++) {
+        line += 'x';
+      }
+      console.log(line);
+    }
+  } else {
+    console.log('Missing size');
+  }
+}

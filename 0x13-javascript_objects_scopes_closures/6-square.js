4a17d38467b4c9bbbeb1990236b4abe1da1f5da1
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-12 22:02:56 +0100

    inherte own class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Shape = require('./5-square');
+
+class Square extends Shape {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;

0964d8c3e3e490db564ca75d5e6eb5be99ee3312
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-15 00:25:46 +0100

    using str format

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    This script connects to a MySQL database.
+    Usage:
+        ./script_name.py <username> <password> <database_name>
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = str(sys.argv[4])
+
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=dbname
+        )
+
+        cur = db.cursor()
+        ex_query = "SELECT * FROM states WHERE name = '{}'".format(state_name)
+        cur.execute(ex_query)
+
+        result = cur.fetchall()
+
+        for row in result:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Database Error: {e}")
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()

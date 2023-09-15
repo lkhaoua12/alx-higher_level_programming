87aef81c51ff59ff38e722fc5a929a6df9184623
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-15 02:08:33 +0100

    join on 2 tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+"""
+    This script connects to a MySQL database and retrieves
+    data from the 'cities' and 'states' tables.
+    It then prints the retrieved data.
+"""
+
+if __name__ == "__main__":
+    # Get command-line arguments for username, password, and dbname.
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # Connect to the MySQL database.
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname
+    )
+
+    # Create a cursor object for executing SQL queries.
+    cur = db.cursor()
+
+    # Execute the SQL query to select data from cities and states tables.
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities \
+                 JOIN states ON cities.state_id = states.id \
+                 ORDER BY states.id ASC')
+
+    # Fetch all the rows from the result set.
+    result = cur.fetchall()
+
+    # Print the retrieved data.
+    for row in result:
+        print(row)

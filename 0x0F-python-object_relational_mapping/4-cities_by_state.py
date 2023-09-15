67594635485b8d58a25d62d3716ab87d63bb4ac3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-15 14:55:24 +0100

    change order by to cities.id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     # Execute the SQL query to select data from cities and states tables.
     cur.execute('SELECT cities.id, cities.name, states.name FROM cities \
                  JOIN states ON cities.state_id = states.id \
-                 ORDER BY states.id ASC')
+                 ORDER BY cities.id ASC')
 
     # Fetch all the rows from the result set.
     result = cur.fetchall()

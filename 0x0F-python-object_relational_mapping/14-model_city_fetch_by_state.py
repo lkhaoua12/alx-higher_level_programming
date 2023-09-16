9f18b718853bd685585fc01f79ef9e346e327710
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-16 22:57:39 +0100

    join two tables

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""join cities on states by states id"""
+
+import sqlalchemy
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    # Setting the conection argument.
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    # Setting up the connection to the MySQL server.
+    url = f'mysql://{user}:{passwd}@localhost:3306/{db}'
+    engine = create_engine(url)
+
+    # Creating a session to interact with the database.
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    # Querying and fetching City objects sorted by cities.id.
+    city_state_info = session.query(State.name, City.id, City.name)\
+                             .join(City)\
+                             .order_by(City.id).all()
+
+    # Displaying the results.
+    for state_name, city_id, city_name in city_state_info:
+        print(f"{state_name}: ({city_id}) {city_name}")
+
+    # Closing the session.
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,9 +6,10 @@ import sys
 from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 # creating the City class.
 class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

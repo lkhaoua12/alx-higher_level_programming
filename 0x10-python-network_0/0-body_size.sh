a8221883b16dde26556a3540cb760eb56f35cbf6
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-29 03:55:52 +0100

    send request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# send the request to url passed as arg
-curl -s "$1" | wc -c
+# Send the request using curl and capture the response in a variable
+response=$(curl -sI "$1")
+# Extract the content length from the response headers
+content_length=$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r\n')
+echo "$content_length"

48b07c4b1fd0e8defe74a2222cf12a4872eebb91
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-30 03:07:44 +0100

    post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send post request with value pair
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

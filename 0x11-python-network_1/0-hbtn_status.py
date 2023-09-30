b4c783f81a9e0977770575bc094cb51719e3d7f5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-30 03:43:44 +0100

    send get requeste and diplay content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" make a get request and diplay content"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        utf_data = data.decode('utf-8')
+        type_data = type(data)
+
+        print(f"\t- type: {type}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {utf_data}")

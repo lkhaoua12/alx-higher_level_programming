8469f357640b1222b277b6ea1c19de5c2aca410a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-30 02:27:13 +0100

    fixed requirmente

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Send the request using curl and capture the response in a variable
-response=$(curl -sI "$1")
-content_length=$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r\n')
-echo "$content_length"
+curl -sw '%{size_download}\n' -o /dev/null "$1"
+

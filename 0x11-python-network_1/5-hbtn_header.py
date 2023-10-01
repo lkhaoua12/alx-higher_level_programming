867c226eee16386e33a059f5f613ea0b47070975
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-01 03:01:37 +0100

    fetch header value fix try

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if r.headers['X-Request-Id']:
+    try:
         print(r.headers['X-Request-Id'])
+    except:
+        pass

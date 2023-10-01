6a399b9c51c39781ded1aa95d345ab36337c1f22
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-01 02:07:52 +0100

    fetch url and check header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,10 @@
 """ fetch url and check header value """
 
 import urllib.request
+import sys
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = sys.argv[1]
 
     # fetching data
     with urllib.request.urlopen(url) as response:

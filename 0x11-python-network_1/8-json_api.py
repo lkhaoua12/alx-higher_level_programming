87939cfa583ea377e277c924d1d9c0fd61fc8f14
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-01 03:35:09 +0100

    fetch from ip

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" fetch data with key=value """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    argp = sys.argv[2]
+
+    try:
+        req = requests.post(url, data=argp)
+        req_json = req.json()
+        if req_json == {}:
+            print('No result')
+        else:
+            print(f'[{req_json.get("id")}] {req_json.get("name")}')
+    except ValueError:
+        print('No result')

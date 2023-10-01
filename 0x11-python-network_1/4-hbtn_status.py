1b34b631a8dffe78d17b2dda0958a7028fdf5ccc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-01 02:39:37 +0100

    fetch data using request module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetch url using request module """
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print('Body response:')
+    print(f'\t- type: {type(r.text)}')
+    print(f'\t- contente: {r.text}')

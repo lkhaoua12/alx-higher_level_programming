1e18c261f63591f21d2d1220add2e7c6cf9c3e02
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-01 03:55:08 +0100

    fetch from api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,25 +8,19 @@ if __name__ == "__main__":
     # Define the URL
     url = "http://0.0.0.0:5000/search_user"
 
-    # Set the default value for the 'q'.
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     else:
         letter = ""
 
-    # Send the POST request with the 'q' parameter
     response = requests.post(url, data={'q': letter})
 
     try:
-        # Try to parse the response JSON
         result = response.json()
 
         if result:
-            # If JSON is valid and not empty, display id and name
             print("[{}] {}".format(result.get('id'), result.get('name')))
         else:
-            # If JSON is empty, display 'No result'
             print("No result")
     except ValueError:
-        # If JSON is invalid, display 'Not a valid JSON'
         print("Not a valid JSON")

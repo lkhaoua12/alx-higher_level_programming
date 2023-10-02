b836fce8c67f34f5b6d9a25e976da32bd67e3fd1
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-02 23:44:53 +0100

    find the peak in a list of int

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Algotithm to find the peak in a list """
+
+
+def find_peak(list_of_integers):
+    """ find the peak in a list of list_of_integers """
+    # Check if the list is empty
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
+
+
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))

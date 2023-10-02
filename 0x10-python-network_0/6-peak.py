8854e33a9ad4fa933dd21ce1b445441c1f3a6768
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-02 23:53:07 +0100

    time space complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -20,12 +20,3 @@ def find_peak(list_of_integers):
             left = mid + 1
 
     return list_of_integers[left]
-
-
-if __name__ == "__main__":
-    print(find_peak([1, 2, 4, 6, 3]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
-    print(find_peak([2, 2, 2]))
-    print(find_peak([]))
-    print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))

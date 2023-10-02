4dd1cbd73870dc611b69f7b8cd23f5ac44025470
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-03 00:38:27 +0100

    fetch user id using api service

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" fetch user id with reqesut module """
+
+
+import sys
+import requests
+
+
+def fetch_user_id(user, passtoken):
+    url = f"https://api.github.com/users/{user}"
+    header = {"Authorization": f"basic {user}:{passtoken}"}
+
+    # sending the requests
+    response = requests.get(url, headers=header)
+
+    # checking response status
+    if response.status_code == 200:
+        u_data = response.json()
+        u_id = u_data.get('id')
+        return u_id
+    return None
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passtoken = sys.argv[2]
+    result = fetch_user_id(user, passtoken)
+    if result is not None:
+        print(result)

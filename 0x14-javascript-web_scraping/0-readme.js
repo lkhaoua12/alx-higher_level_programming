6495dacad20c321731f09679572e0d3b20a2116b
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-25 03:38:41 +0100

    fixed file permission

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@
 const myArgs = process.argv.slice(2);
 const fs = require('fs');
 fs.readFile(myArgs[0], 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data);
+	}
 });

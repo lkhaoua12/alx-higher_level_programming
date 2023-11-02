998000a09d4e031a19f1100dab5806475e175ca8
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-11-02 01:57:23 +0100

    fixed Cors problem

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -3,9 +3,12 @@
   * fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from the fetch in the HTML tag DIV#hello
   * you must use JQuery
   */
-const $ = window.$;
-const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
-$.get(url, function (data) {
-  const hello = data.hello;
-  $('DIV#hello').text(hello);
+$(document).ready(function() {
+  // Use jQuery to fetch data from the URL
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr' 
+  $.get(url, function(data) {
+    const translation = data.hello;
+    $('DIV#hello').text(translation);
+  });
 });
+
